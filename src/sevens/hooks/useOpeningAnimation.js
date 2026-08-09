c8d24d7c615c2d36d5aa269895d573b254d1a9ac
eieiouka <eieiyukkuri@gmail.com>
2026-08-09 13:00:21 +0900

    j

--- src/sevens/hooks/useOpeningAnimation.js
+++ src/sevens/hooks/useOpeningAnimation.js
@@ -70,7 +70,7 @@ export default function useOpeningAnimation({
       着地した飛行カードを、
       その場に表示しておく時間。
     */
-    const landingHoldDuration = 200;
+    const landingHoldDuration = 400;
 
     orderedSevens.forEach(
       (card, index) => {
